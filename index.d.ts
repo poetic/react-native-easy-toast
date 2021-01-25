2f7889ae0249ae65a40bf01f0c30abcd57cdc945
David Clausen <david@poetic.io>
2021-01-25 11:51:53 -0600

    improve typing with prop interface

--- index.d.ts
+++ index.d.ts
@@ -1,11 +1,21 @@
 import { Component, ReactNode } from "react";
 
+interface ToastComponentProps {
+  fadeInDuration?: number;
+  fadeOutDuration?: number;
+  opacity?: number;
+  pointerEvents?: string
+  position?: "bottom" | "center" | "top";
+  positionValue?: number;
+  textStyle?: {};
+}
+
 declare module "react-native-easy-toast" {
   export interface DURATION {
     LENGTH_SHORT: number;
     FOREVER: number;
   }
-  export default class Toast extends Component<{ pointEvents?: string }> {
+  export default class Toast extends Component<ToastComponentProps> {
     show: (
       text: string | ReactNode,
       duration?: number,
